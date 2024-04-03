5c2e0583ca2c725530a7dd0ec2ea632078f5b410
Jo Martin Slåtten <jomartinslaatten98@hotmail.com>
2024-04-03 16:52:21 +0200

    Add API endpoints for retrieving information about floors, rooms, devices, and sensors

--- smarthouse/api.py
+++ smarthouse/api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from random import random
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -53,16 +55,103 @@ def get_smarthouse_info() -> dict[str, int | float]:
 # here ...
 
 
-@app.get("/smarthouse/floors")
-def get_floors():
-    info = []
-    for i in range(len(smarthouse.get_floors())):
-        info.append(f"Floor {i + 1} has {len(smarthouse.get_floors()[i].rooms)} rooms")
-    
-    return info 
+@app.get("/smarthouse/floor")
+def get_floors() -> dict[str, int | float | str]:
+    info = {}
+    floors = smarthouse.get_floors()  # Antar denne metoden returnerer en liste med etasjer
+    for i, floor in enumerate(floors):
+        key = f"floor_no {i+1}, no of rooms "
+        info[key] = len(floor.rooms)
 
+    return info
+
+
+@app.get("/smarthouse/floor/{fid}")
+def get_floor(fid: int) -> dict[str, int | float | str]:
+    floor = smarthouse.get_floors()[fid-1]
+    return {
+        "floor_no": fid,
+        "no_rooms": len(floor.rooms),
+    }
+
+
+@app.get("/smarthouse/floor/{fid}/room")
+def get_rooms(fid: int) -> dict[str, int | float | str]:
+    floor = smarthouse.get_floors()[fid-1]
+    rooms = floor.rooms
+    info = {}
+    for i, room in enumerate(rooms):
+        key = f"room_no {i+1}, area "
+        info[key] = room.room_size
+    return info
+
+
+@app.get("/smarthouse/floor/{fid}/room/{rid}")
+def get_room(fid: int, rid: int) -> dict[str, int | float | str]:
+    floor = smarthouse.get_floors()[fid-1]
+    room = floor.rooms[rid-1]
+    return {
+        "room_name": room.room_name,
+        "room_no": rid,
+        "area": room.room_size,
+    }
+
+
+@app.get("/smarthouse/device")
+def get_devices():
+    info = []
+    for i in range(len(smarthouse.get_devices())):
+        info.append({"device": i+1, "room": smarthouse.get_devices()[i].room.room_name})
+
+    return info
+
+@app.get("/smarthouse/device/{uuid}")
+def get_device(uuid: str):
+    for i in range(len(smarthouse.get_devices())):
+        if smarthouse.get_devices()[i].id == uuid:
+            return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name}
+
+    return {"error": "device not found"}
+
+
+@app.get("/smarthouse/sensor/{uuid}/current")
+def get_sensor(uuid: str):
+    for i in range(len(smarthouse.get_devices())):
+        if smarthouse.get_devices()[i].id == uuid:
+            return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name, "temperature": repo.get_latest_reading(smarthouse.get_devices()[i])}
+        
+    return {"error": "device not found"}
+
+@app.post("/smarthouse/sensor/{uuid}/current")
+def post_sensor(uuid: str):
+    for i in range(len(smarthouse.get_devices())):
+        if smarthouse.get_devices()[i].id == uuid:
+            c = repo.conn.cursor()
+            c.execute(f"INSERT INTO measurements (device, ts, value, unit) VALUES ('{uuid}', '{datetime.now().isoformat()}', {random() * 10}, '{smarthouse.get_devices()[i].unit}');")
+            repo.conn.commit()
+            c.close()
+
+
+@app.get("/smarthouse/sensor/{uuid}/values")
+def get_sensor_values(uuid: str, limit: int):
+
+    if limit:
+        c = repo.conn.cursor()
+        c.execute(f"SELECT ts, value, unit FROM measurements WHERE device = '{uuid}' ORDER BY ts DESC LIMIT {limit};")
+    else:
+        c = repo.conn.cursor()
+        c.execute(f"SELECT ts, value, unit FROM measurements WHERE device = '{uuid}';")
+    c.close()
+    return c.fetchall()
+
+
+@app.delete("/smarthouse/sensor/{uuid}/oldest")
+def delete_sensor_values(uuid: str):
+    c = repo.conn.cursor()
+    c.execute(f"DELETE FROM measurements WHERE device = '{uuid}' ORDER BY ts ASC LIMIT 1;")
+    repo.conn.commit()
+    c.close()
 
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
